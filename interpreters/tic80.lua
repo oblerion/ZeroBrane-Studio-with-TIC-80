bcbbef6b35a77b3f535d185d8741bd7392144743
Magnus Oblerion <82583559+oblerion@users.noreply.github.com>
2024-08-23 12:31:34 +0200

    fix and opti tic80.lua

--- interpreters/tic80.lua
+++ interpreters/tic80.lua
@@ -1,75 +1,42 @@
 -- file: interpreters/tic80.lua
 --[[--
-    TIC-80 API for ZBStudio
+    TIC-80 Interpreter for ZBStudio
     @see [TIC-80 wiki](https://github.com/nesbox/TIC-80/wiki/)
+    Version 1.0
 --]]--
+local win = ide.osname == "Windows"
+local mac = ide.osname == "Macintosh"
 local cartext = "tic"
 local srcext = "lua"
 
 
 local function locateexe()
   -- TODO: look up the executable (see love2d.lua and others)
-  return ide.config.path.tic80
-end
-
-
-local function locatecart(wfilename, args)
-  local fullpath = wfilename:GetFullPath()
-  
-  -- Check if cart is among command line params
-  if string.find(args, "[^%.%s]%." .. cartext .. "$") or
-      string.find(args, "[^%.%s]%." .. cartext .. "%s") then
-    return nil
-  end
-  
-  -- Check if running a cart
-  if GetFileExt(fullpath) == cartext then
-    return fullpath
-  end
-  
-  -- Look for a cart with the same name as active file.
-  local guesspath
-  if GetFileExt(fullpath) then
-    guesspath = string.gsub(fullpath, "%.([^./\\]*)$", "." .. cartext)
+  local lpath
+  if ide.config.path.tic80~=nil then
+    lpath = ide.config.path.tic80
+  elseif win then
+    lpath = "C:/Program Files (x86)/Tic80/tic80.exe"
+  elseif mac then
+    
   else
-    guesspath = fullpath .. "." .. cartext
+    lpath = "/usr/bin/tic80"
   end
-  if wx.wxFileExists(guesspath) then
-    return guesspath
-  end
-  
-  -- Look for any cart in the same dir as the source file
-  local nearest = wx.wxFindFirstFile(GetPathWithSep(wfilename) .. "*." .. cartext , wx.wxFILE)
-  if wx.wxFileExists(nearest) then
-    return nearest
-  end
+  return lpath
 end
 
 
-local function srcarg(wfilename, args)
-  -- Check if something is already in command line params
-  if string.find(args, "%s-code")then -- covers `-code` and `-code-watch`
-    return nil
-  end
-  
-  -- Check if the current file is source
-  local active = wfilename:GetFullPath()
-  if GetFileExt(active) == srcext then
-    return ("-code %s"):format(active)
-  end
-end
-
-
--- join non-empty args with 1 space
-local function command(...)
-  local buf = ""
-  for _, str in pairs({...}) do
-    if str and str ~= "" then 
-      local addspace = (#buf > 0 and string.sub(buf, -1) ~= " ")
-      buf = buf .. (addspace and " " or "") .. str
+local function locatecart(wfilename)--, args)
+  local fullpath = wfilename:GetFullPath()
+  local scart=fullpath
+  for i=fullpath:len(),1,-1 do
+    if fullpath[i]=='.' then
+      local path1 = fullpath:sub(1,fullpath:len()-3)
+      scart = path1..".tic"
+      break
     end
   end
-  return buf
+  return scart
 end
 
 
@@ -80,9 +47,8 @@ return {
   luaversion="5.3",
   frun=function(self,wfilename)
     local tic80 = locateexe()
-    local args = self:GetCommandLineArg() or ""
-    local cart = locatecart(wfilename, args)
-    local code = srcarg(wfilename, args)
+    local cart = locatecart(wfilename)
+    local code = wfilename:GetFullPath()
     
     if not tic80 then
       ide:Print("TIC-80 executable not found. Please add `path.tic80` in the config file (`Edit > Preferences`)")
@@ -96,7 +62,7 @@ return {
       ide:Print("No source file specified. The code from the cart will run. To use a source file open it in the editor, or add `-code /path/to/file.lua` in `Project > Command Line Parameters...`")
     end
     
-    local cmd = string.format(tic80.." "..cart.." --cmd=%cimport code %s%c",'"',code,'"') --command(tic80, cart, code, args)
+    local cmd = string.format(tic80.." "..cart.." --cmd="..'"'.."import code %s"..'"',code)
     return CommandLineRun(cmd,self:fworkdir(wfilename),true,true,nil,nil,nil)
   end,
   skipcompile = true,
